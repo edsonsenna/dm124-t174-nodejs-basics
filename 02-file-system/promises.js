49f8b2116f939ca40fd15df39fe1a93052bc5beb
e <e>
2019-10-19 16:58:08 -0300

    Concating Promises

--- 02-file-system/promises.js
+++ 02-file-system/promises.js
@@ -1,11 +1,18 @@
 function bakeACake() {
     return new Promise((resolve, reject) => {
         console.log('Starting baking the cake!');
-        setTimeout(() => resolve('Cake is ready!'), 2000);
-        reject('No cake today!');
+        setTimeout(() => resolve('Cake is ready'), 500);
+        //reject('No cake today!');
+    });
+}
+
+function addFronsting(cake) {
+    return new Promise((resolve, reject) => {
+        resolve(cake + ' with a delicious vanilla frosting');
     });
 }
 
 bakeACake()
+    .then(addFronsting)
     .then((data) => console.log(data))
     .catch((err) => console.log(err));
