71e4036161a8c5522578d82ba28bf715c90d7061
e <e>
2019-10-19 13:56:53 -0300

    Adding Typeof Number Verification

--- 01-javascript-101/sum.js
+++ 01-javascript-101/sum.js
@@ -3,14 +3,16 @@ let numberB = 31;
 
 const sum = numberA + numberB;
 
-console.log(`The sum is: ${ sum }`);
+console.log(`The sum is: ${sum}`);
 
 const argvFirstElement = 2;
 const argvLastElement = process.argv.length - 1;
 
 let numbers = process.argv.splice(argvFirstElement, argvLastElement);
 
-let sumNumbers = numbers
-                .reduce((previousValue, currentValue) => parseInt(previousValue)+parseInt(currentValue));
+let sumArgvNumbers = numbers
+                        .filter( value => Number(value) ? Number(value) : Number(0))
+                        .reduce((previousValue, currentValue) =>
+                        Number(previousValue) + Number(currentValue));
 
-console.log(`The sum is: ${ sumNumbers }`);
+console.log(`The sum of argv's numbers is: ${sumArgvNumbers}`);
