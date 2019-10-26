911016e22f0d8f5f88c3884bd79fb73ff7de8790
e <e>
2019-10-26 14:55:52 -0300

    Print Names With Colors

--- 04-npm/index.js
+++ 04-npm/index.js
@@ -1,1 +1,18 @@
-console.log('\x1b[33m%s\x1b[0m', 'Hi!');
+const chalk = require('chalk');
+const string = require('useful-string');
+
+const names = [
+    'Edson de Senna Júnior',
+    'Filipe Henrique Benjamin',
+    'Mário Guilherme Macedo',
+    'André Luiz Oneti Carvalho'
+]
+
+const print = (name, index) => {
+    console.log(chalk.default[index % 2 === 0 ? 'blue' : 'yellow'](name));
+}
+
+names
+    .sort()
+    .map(string.hyphenate)
+    .forEach(print);
